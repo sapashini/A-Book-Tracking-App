b28bd33914e5b21841a29ad33617aa910e166eba
Amin Alhassan <alaasan@yahoo.com>
2019-02-05 17:10:20 -0500

    Change package jason

---
+++ build/precache-manifest.76ab5f8b9a9ab68d357acf646e12742a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/A-Book-Tracking-App/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/A-Book-Tracking-App/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/A-Book-Tracking-App/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "8d0674c0ade7d65efa16",
+    "url": "/A-Book-Tracking-App/static/js/runtime~main.8d0674c0.js"
+  },
+  {
+    "revision": "8aa9575dc10c4ea4a1ae",
+    "url": "/A-Book-Tracking-App/static/js/main.8aa9575d.chunk.js"
+  },
+  {
+    "revision": "f567cd14846ca13d3e3d",
+    "url": "/A-Book-Tracking-App/static/js/1.f567cd14.chunk.js"
+  },
+  {
+    "revision": "8aa9575dc10c4ea4a1ae",
+    "url": "/A-Book-Tracking-App/static/css/main.220a3a72.chunk.css"
+  },
+  {
+    "revision": "c7a04f1b90280187851aa5c52fca670f",
+    "url": "/A-Book-Tracking-App/index.html"
+  }
+];
